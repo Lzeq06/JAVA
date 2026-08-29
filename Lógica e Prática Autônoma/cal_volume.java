81a5d13551722de2cf9f69d983b1861d184f8644
Luara Zequini <luarazequini5@gmail.com>
2026-08-29 22:22:38 +0000

    feat(java): implementa cal_volume.java

---
+++ Lógica e Prática Autônoma/cal_volume.java
@@ -0,0 +1,35 @@
+
+//Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula:
+//VOLUME = 3.14159 * R2 * ALTURA
+
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String[] args) {
+
+
+    Scanner input = new Scanner(System.in);
+
+        //declarar dados 
+
+        System.out.print("Digite o valor do raio:");
+        double raio = input.nextDouble();
+
+          System.out.print("Digite o valor da altura:");
+            double altura = input.nextDouble();
+
+            double volume = 3.14159 * (raio * raio) * altura;
+
+        
+        System.out.printf("O valor do volume é: %.2f", volume);
+
+        input.close();
+
+        
+            
+        
+        
+       
+    }
+}
