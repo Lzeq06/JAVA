0c6f7c5485263b4e1c4e0aa92ade55a066ca5e7b
Luara Zequini <luarazequini5@gmail.com>
2026-08-29 22:22:37 +0000

    feat(java): implementa Número de Diagonais de um Polígono Convexo.java

---
+++ Geometria/Número de Diagonais de um Polígono Convexo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextLong()) {
+            long n = sc.nextLong();
+            System.out.println(n * (n - 3) / 2);
+        }
+        sc.close();
+    }
+}
